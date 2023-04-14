ebd466e5337535fa825eaba01dc15b5b3b952a6b
Saswata Banerjee <azure.saswata@gmail.com>
2023-04-14 11:32:20 +0530

    useEffect with fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
-import { useState } from "react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
   const [state, setState] = useState({ count: 4, theme: "blue" });
   const count = state.count;
   const theme = state.theme;
   const [resourceType, setResourceType] = useState("posts");
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    // console.log('render '+resourceType);
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, [resourceType]);
 
   const decrementCount = () => {
     if (count > 1)
@@ -32,13 +39,26 @@ function App() {
       <button onClick={incrementCount}>+</button>
       <hr />
       <div>
-        <button onClick={() => setResourceType('posts')}>Posts</button>
-        <button onClick={() => setResourceType('users')}>Users</button>
-        <button onClick={() => setResourceType('comments')}>Comments</button>
+        <button onClick={() => setResourceType("posts")}>Posts</button>
+        <button onClick={() => setResourceType("users")}>Users</button>
+        <button onClick={() => setResourceType("comments")}>Comments</button>
       </div>
       <h1>{resourceType}</h1>
+      {items.map((item) => {
+        return (
+          <div key={item.id}>
+            <h2>{item.title}</h2>
+            {item.email && <h4>
+              {item.name}{" "}
+              <span style={{ color: "blue" }}>&lt;{item.email}&gt;</span> 
+              {item.postId && <>commented on <span style={{color:'red'}}>post id {item.postId}</span></>}
+            </h4>}
+            <p>{item.body}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
 
-export default App;
+// export default App;
