800896be3449bcee7ae6567ea15f2d136f73caa9
Saswata Banerjee <azure.saswata@gmail.com>
2023-04-14 18:31:08 +0530

    useMemo comes to rescue!

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import "./App.css";
 import React, { useMemo, useState } from "react";
 
 export default function App() {
-  const [number, setNumber] = useState(0);
+  const [number, setNumber] = useState(20);
   const [dark, setDark] = useState(false);
-  const doubleNumber = slowFunction(number);
+  const doubleNumber = useMemo(() => {
+    return slowFunction(number);
+  }, [number]);
 
+  // dont memoize everything as it has a memory overhead
   const themeStyles = {
-    backgroundColor: dark ? "black" : "white",
-    color: dark ? "white" : "black",
+    backgroundColor: dark ? "black" : "tomato",
+    color: dark ? "tomato" : "black",
+    textAlign: 'center',
+    height: (doubleNumber+20)+'px',
+    width: '50%',
+    lineHeight: (doubleNumber+20)+'px',
+    margin: '0 auto'
   };
 
   return (
@@ -29,5 +37,5 @@ export default function App() {
 function slowFunction(num) {
   console.log("Calling slow function");
   for (let i = 0; i < 1000000000; i++) {}
-  return num * 2;
+  return num * 4;
 }
