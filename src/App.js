e5742dbddfc42b74a3ebb50941da01e2c10bf42e
Saswata Banerjee <azure.saswata@gmail.com>
2023-04-14 11:00:31 +0530

    multiple state vars

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import './App.css';
+import "./App.css";
 import { useState } from "react";
 import React from "react";
 
 function App() {
-  const [count, setCount] = useState(() => {
-    console.log("runs only first time")
-    return 4;
-  });
+  const [state, setState] = useState({ count: 4, theme: "blue" });
+  const count = state.count;
+  const theme = state.theme;
 
   const decrementCount = () => {
-    if(count > 1)setCount(prevCount => prevCount - 1);
-  }
+    if (count > 1)
+      setState((prevState) => {
+        return { ...prevState, count: prevState.count - 1, theme: "blue" };
+      });
+  };
 
   const incrementCount = () => {
-    setCount(prevCount => prevCount + 1);
-  }
+    setState((prevState) => {
+      return { ...prevState, count: prevState.count + 1, theme: "red" };
+    });
+  };
 
   return (
     <div className="App">
+      <span style={{ background: theme, color: "white", padding: '0 20px' }}>THEME</span><br></br>
       <button onClick={decrementCount}>-</button>
-      <span style={{padding:"0 20px"}}>{count}</span>
+      <span style={{ padding: "0 20px", color: theme }}>{count}</span>
       <button onClick={incrementCount}>+</button>
     </div>
   );
