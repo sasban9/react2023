16f27cf129befcb1d8d4d4d18f8645b9681e80da
Saswata Banerjee <azure.saswata@gmail.com>
2023-04-15 08:07:15 +0530

    useUpdateLogger added

--- src/components/AppCustomHook.js
+++ src/components/AppCustomHook.js
@@ -1,8 +1,10 @@
 // import { useState } from "react";
 import useLocalStorage from "../useLocalStorage";
+import useUpdateLogger from "../useUpdateLogger";
 
 export default function App() {
   const [name, setName] = useLocalStorage("name", "");
+  useUpdateLogger(name);
 
   return (
     <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
