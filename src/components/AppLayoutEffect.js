a48a15a82afa6cc88a2cedae3111ee554c6589bb
Saswata Banerjee <azure.saswata@gmail.com>
2023-04-15 08:23:40 +0530

    useLayoutEffect demo

---
+++ src/components/AppLayoutEffect.js
@@ -0,0 +1,26 @@
+import { useLayoutEffect, useRef, useState } from "react";
+
+export default function App() {
+  const [show, setShow] = useState(false);
+  const popup = useRef();
+  const button = useRef();
+  useLayoutEffect(() => {
+    if (popup.current == null || button.current == null) return;
+    const { bottom } = button.current.getBoundingClientRect();
+    popup.current.style.top = `${bottom + 25}px`;
+  }, [show]);
+
+  return (
+    <>
+    <p>useLayoutEffect Demo</p>
+      <button ref={button} onClick={() => setShow((prev) => !prev)}>
+        Click Here
+      </button>
+      {show && (
+        <div style={{ position: "absolute", background: "khaki", padding:10 }} ref={popup}>
+          This is a popup
+        </div>
+      )}
+    </>
+  );
+}
